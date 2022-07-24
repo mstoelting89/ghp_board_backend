0c443c9b1589dc25676053d660205dbda7af3a5f
mstoelting89 <michaelstoelting@gmail.com>
2022-07-24 18:58:28 +0200

    implement contact function

---
+++ system/src/main/java/com/example/system/contact/ContactController.java
@@ -0,0 +1,48 @@
+package com.example.system.contact;
+
+import com.example.system.email.EmailService;
+import lombok.AllArgsConstructor;
+import org.h2.jdbc.meta.DatabaseMetaServer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin
+public class ContactController {
+
+    private EmailService emailService;
+    private SpringTemplateEngine springTemplateEngine;
+
+    @PostMapping(path = "/api/v1/contact")
+    public ResponseEntity<?> sendContactMail(@RequestBody ContactDto contactDto) {
+
+        if (emailService.checkEmail(contactDto.getEmail())) {
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("email", contactDto.getEmail());
+            model.put("name", contactDto.getLastName());
+            model.put("vorname", contactDto.getFirstName());
+            model.put("text", contactDto.getMessage());
+            Context context = new Context();
+            context.setVariables(model);
+
+            String html = springTemplateEngine.process("new-contact", context);
+
+            emailService.send("michaelstoelting@gmail.com", html, "Guitar Hearts Project: Neue Kontaktanfrage");
+            return new ResponseEntity<>("Kontaktnachricht wurde erfolgreich verschickt", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Bitte geben Sie eine korrekte Email-Adresse an", HttpStatus.OK);
+        }
+
+    }
+}
--- system/src/main/java/com/example/system/email/EmailService.java
+++ system/src/main/java/com/example/system/email/EmailService.java
@@ -2,4 +2,6 @@ package com.example.system.email;
 
 public interface EmailService {
     void send(String to, String email, String subject);
+
+    boolean checkEmail(String email);
 }
