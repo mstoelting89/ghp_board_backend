68cf70ad86955620aa8ee4c0d68179d77875a7ae
mstoelting89 <michaelstoelting@gmail.com>
2023-01-06 09:10:55 +0100

    - change phone number

--- system/src/test/java/com/example/system/blog/BlogServiceTest.java
+++ system/src/test/java/com/example/system/blog/BlogServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.system.blog;
 
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -8,5 +9,23 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 @SpringBootTest
 public class BlogServiceTest {
-
+    @AfterEach
+    public void clearTestDatabaseFromEntries() {
+        
+    }
+    
+    //TODO: shouldGetOneSpecificBlogEntry
+    //TODO: shouldGetAllBlogEntries
+    //TODO: shouldInsertNewBlogEntryWithImage
+    //TODO: shouldInsertNewBlogEntryWithSeveralImages
+    //TODO: shouldInsertNewBlogEntryWithoutImage
+    //TODO: shouldReturnFailureIfDataIsMissingWhenInsertingData
+    //TODO: shouldUpdateExistingBlogEntryWithDifferentData
+    //TODO: shouldUpdateExistingBlogEntryWithNewImage
+    //TODO: shouldUpdateExistingBlogEntryWithNewImageWhileHavingExistingImages
+    //TODO: shouldRemoveImageFromExistingBlogEntry
+    //TODO: shouldRemoveImageFromExistingBlogEntryWhileHavingSeveralExistingImages
+    //TODO: shouldDeleteBlogEntryWithoutImage
+    //TODO: shouldDeleteBlogEntryWithImage
+    //TODO: shouldDeleteBlogEntryWithSeveralImages
 }
